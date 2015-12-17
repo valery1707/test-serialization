4e1ae160fcf2e2cac25acb6d875e3e3cfecde03c
Valeriy.Vyrva <valery1707@gmail.com>
2015-12-17 17:44:55 +0600

    Проверка работы тестов

---
+++ serialization/src/test/java/name/valery1707/test/serialization/util/EnvironmentTest.java
@@ -0,0 +1,13 @@
+package name.valery1707.test.serialization.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class EnvironmentTest {
+
+	@Test
+	public void simpleTest() {
+		assertTrue(true);
+	}
+}
