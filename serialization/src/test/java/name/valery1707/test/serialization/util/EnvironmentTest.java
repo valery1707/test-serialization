e947e325c8fca9d46c20e3d275206ccd97163682
Valeriy.Vyrva <valery1707@gmail.com>
2015-12-17 17:53:39 +0600

    Используем AssertJ для удобного тестирования

--- serialization/src/test/java/name/valery1707/test/serialization/util/EnvironmentTest.java
+++ serialization/src/test/java/name/valery1707/test/serialization/util/EnvironmentTest.java
@@ -2,6 +2,9 @@ package name.valery1707.test.serialization.util;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class EnvironmentTest {
@@ -9,5 +12,19 @@ public class EnvironmentTest {
 	@Test
 	public void simpleTest() {
 		assertTrue(true);
+	}
+
+	@Test
+	public void testAssertJ() {
+		assertThat(Arrays.asList("1", "2", "3"))
+				.isNotEmpty()
+				.hasSize(3)
+				.isSorted()
+				.containsOnly("3", "1", "2")
+				.doesNotContain("4")
+				.extracting(Integer::valueOf)
+				.doesNotContainNull()
+				.isSubsetOf(Arrays.asList(1, 2, 3, 4, 5))
+		;
 	}
 }
